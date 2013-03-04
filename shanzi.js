108fb35ff996aa615a95917dced8d7900692190d
FENG Zhichao <flankerfc@gmail.com>
2013-03-05 00:34:11 +1100

    rotating the fan.

--- shanzi.js
+++ shanzi.js
@@ -4,10 +4,12 @@ $(function () {
     $('.shanzi li').each(function (index) {
       var offset = (startIndex + index) % 24;
       var deg = 15 * offset;
+      var zIndex = deg === 0 ? 360 : (360 - deg) % 360;
       $(this).css({
         '-webkit-transform': 'rotate(' + deg  + 'deg)',
         '-moz-transform': 'rotate(' + deg  + 'deg)',
-        'ms-transform': 'rotate(' + deg  + 'deg)'
+        'ms-transform': 'rotate(' + deg  + 'deg)',
+        'z-index': zIndex
       });
     });
   };
@@ -15,7 +17,8 @@ $(function () {
   rotate(0);
 
   $('.shanzi li').click(function () {
-
+    var startIndex = $(this).index();
+    rotate(-startIndex);
   });
 
 });
